1324235fbf8e113891e8533e71c82314f067c2df
Audun Hilden <audunhilden@live.no>
2020-11-10 15:38:34 +0100

    lower > upper

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Default from '~/ui/layouts/default'
+import Default from '~/ui/layouts/Default'
 import '~/public/css/style.css'
 
 function App({ Component, pageProps, router }) {
