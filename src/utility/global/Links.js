4c6e957b054adbbea1d7d4b1d614422e39eaf9dc
Jonfirexbox <66625200+Jonfirexbox@users.noreply.github.com>
2020-11-10 12:06:28 -0500

    Update Links.js
    Helpful? I think

--- src/utility/global/Links.js
+++ src/utility/global/Links.js
@@ -1,1 +1,1 @@
-export const Twitter = "https://twitter.com/tweetaudun";
+export const Twitter = "https://twitter.com/tweetaudun"; //check our "https://twitter.com/discordlistgg" Audun owns that twitter as well
