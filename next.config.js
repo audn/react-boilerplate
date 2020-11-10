e3fb43f0605b7a8e3cc1fcad20d79f571d1f220e
Audun Hilden <audunhilden@live.no>
2020-11-10 15:33:31 +0100

    test, possible fix on vercel

--- next.config.js
+++ next.config.js
@@ -4,12 +4,17 @@ const withAlias = require('@blunck/next-alias')({
 })
 
 module.exports = withAlias({
-  webpack: (cfg) => {
+  webpack: (cfg, { isServer }) => {
     cfg.module.rules.push({
       test: /\.ya?ml$/,
       type: 'json',
       use: 'yaml-loader'
     })
+    if (!isServer) {
+      cfg.node = {
+        fs: 'empty'
+      }
+    }
     return cfg
   }
 })
