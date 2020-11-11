63f9d5ce6fb5c4e76c8ccc42b9835581e4e66851
Audun Hilden <audunhilden@live.no>
2020-11-11 10:10:31 +0100

    test again

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,3 @@
 module.exports = {
-  plugins: {
-    tailwindcss: {},
-    'postcss-preset-env': {},
-    'postcss-nested': {},
-    ...(process.env.NODE_ENV === `production`
-      ? {
-          '@fullhuman/postcss-purgecss': {
-            content: [`./src/**/*.js`],
-            defaultExtractor: (content) =>
-              content.match(/[A-Za-z0-9-_:/]+/g) || []
-          },
-          autoprefixer: {}
-        }
-      : {})
-  }
+  plugins: [require('tailwindcss')('./tailwind-config.js')]
 }
