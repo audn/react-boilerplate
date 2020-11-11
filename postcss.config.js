27980df2469bc778e1d3a455518c21fffc345a03
Audun Hilden <audunhilden@live.no>
2020-11-11 10:23:54 +0100

    test

--- postcss.config.js
+++ postcss.config.js
@@ -5,11 +5,6 @@ module.exports = {
     'postcss-nested': {},
     ...(process.env.NODE_ENV === `production`
       ? {
-          '@fullhuman/postcss-purgecss': {
-            content: [`./src/**/*.js`],
-            defaultExtractor: (content) =>
-              content.match(/[A-Za-z0-9-_:/]+/g) || []
-          },
           autoprefixer: {}
         }
       : {})
