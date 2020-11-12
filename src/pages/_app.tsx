41662c5d90d3b17b0adfcaa6d66948f391641e58
Audun Hilden <audunhilden@live.no>
2020-11-12 12:38:13 +0100

    Favicon & Async configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,18 +3,24 @@ import Head from 'next/head';
 import { GA_TRACKING_ID } from '../utility/data/Analytics';
 import { AppProps } from 'next/app';
 import '../assets/style.css';
+{
+  /* 
+  Favicon = https://realfavicongenerator.net
+  Don't forget this! https://i.imgur.com/SrXVxHw.png
 
+  Structure:
+  - Title
+  - Fonts / Icons
+  - Meta / Theme colors
+  - Favicon
+  - Scripts
+  */
+}
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <Fragment>
       <Head>
         <title>MyApp</title>
-        <script
-          src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js"
-          async
-          crossOrigin="anonymous"
-        />
-
         <link
           rel="preload"
           as="style"
@@ -22,33 +28,55 @@ export default function App({ Component, pageProps, router }: AppProps) {
           data-onload="if(media!='all')media='all'"
           href="https://rawgit.com/ellekasai/twemoji-awesome/gh-pages/twemoji-awesome.css"
         />
-
         <link
-          rel="preload"
-          as="style"
-          media="none"
-          data-onload="if(media!='all')media='all'"
+          rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500&display=swap"
         />
-
         <link
-          rel="preload"
-          as="style"
-          media="none"
-          data-onload="if(media!='all')media='all'"
+          rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
         />
-
         <link
-          rel="preload"
-          as="style"
-          media="none"
-          data-onload="if(media!='all')media='all'"
+          rel="stylesheet"
           href="https://pro.fontawesome.com/releases/v5.13.1/css/all.css"
           integrity="sha384-B9BoFFAuBaCfqw6lxWBZrhg/z4NkwqdBci+E+Sc2XlK/Rz25RYn8Fetb+Aw5irxa"
           crossOrigin="anonymous"
         />
-
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff" />
+        <link rel="shortcut icon" href="/assets/favicon/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/assets/favicon/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/assets/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/assets/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/assets/favicon/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="/assets/favicon/safari-pinned-tab.svg"
+          color="#5bbad5"
+        />
+        <meta
+          name="msapplication-config"
+          content="/assets/favicon/browserconfig.xml"
+        />
+        <script
+          src="https://twemoji.maxcdn.com/v/latest/twemoji.min.js"
+          async
+          crossOrigin="anonymous"
+        />
         <script
           async
           src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
@@ -62,9 +90,7 @@ export default function App({ Component, pageProps, router }: AppProps) {
     					gtag('config', '${GA_TRACKING_ID}');
         		`,
           }}
-        />
-        <meta name="msapplication-TileColor" content="#7649f9" />
-        <meta name="theme-color" content="#7649f9" />
+        ></script>
       </Head>
       <Component {...pageProps} key={router.route} />
     </Fragment>
