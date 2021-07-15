155eec3364df899d8420765632ad3c18551075e1
Audun Hilden <audunhilden@live.no>
2021-07-15 17:43:42 +0200

    refactor: 301 redirect to protect nested components inside /:page/components

--- next.config.js
+++ next.config.js
@@ -2,4 +2,56 @@ module.exports = {
   images: {
     domains: ['images.unsplash.com'],
   },
+  module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/(.*)/components/(.*)',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/(.*)/screens/(.*)',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/(.*)/modals/(.*)',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/(.*)/(.*)/components',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/(.*)/(.*)/screens',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/(.*)/(.*)/modals',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/(.*)/(.*)/(.*)/components',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/(.*)/(.*)/(.*)/screens',
+        destination: '/',
+        permanent: true,
+      },
+      {
+        source: '/(.*)/(.*)/(.*)/modals',
+        destination: '/',
+        permanent: true,
+      },
+    ];
+  },
+};
+
 };
