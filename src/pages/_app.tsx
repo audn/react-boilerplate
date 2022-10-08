3905a312307d91abc0bc613ab93d74864ad5d46d
Audun Hilden <audunhilden@live.no>
2022-10-08 09:34:04 +0200

    fix<SEO>: forgot to initialize default SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,17 @@ import { AppProps } from 'next/app';
 import '../assets/css/style.css';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { DefaultSeo } from 'next-seo';
+import SEO from '../../next-seo.config';
 import Footer from '../common/components/Footer';
 import { Header } from '../common/components/Header';
-
 const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
+      <DefaultSeo {...SEO} />
       <div className="mt-24">
         <Component {...pageProps} key={router.route} />
       </div>
