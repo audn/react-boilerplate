16f216b84fc29b2050c1162ca2861fa6d91e27c6
Audun Hilden <audunhilden@live.no>
2022-10-09 19:21:06 +0200

    choir: removed fontawesome link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -12,12 +12,7 @@ export default class extends Document {
             crossOrigin="anonymous"
             strategy="beforeInteractive"
           />
-          <link
-            rel="stylesheet"
-            href="https://use.fontawesome.com/releases/v5.15.4/css/all.css"
-            integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm"
-            crossOrigin="anonymous"
-          />
+
           <meta name="application-TileColor" content="#4d7fff" />
           <meta name="theme-color" content="#4d7fff" />
           <link rel="shortcut icon" href="/favicon/favicon.ico" />
