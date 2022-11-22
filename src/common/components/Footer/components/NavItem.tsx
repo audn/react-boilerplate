c55467a91c4af18eab1dc16799f1ee330416a606
Audun Hilden <audunhilden@live.no>
2022-11-22 05:41:29 +0100

    fix: build error

--- src/common/components/Footer/components/NavItem.tsx
+++ src/common/components/Footer/components/NavItem.tsx
@@ -5,11 +5,12 @@ function NavItem({ label, route }: INavItem) {
   return (
     <li>
       <Link
-        href={route}
+        href={route ?? '#'}
         className="inline-flex items-center gap-2 rounded-sm hover:underline text-white/70 hover:text-black dark:hover:text-white focus-visible:ariakit-outline"
       >
         {label}
       </Link>
+      s
     </li>
   );
 }
