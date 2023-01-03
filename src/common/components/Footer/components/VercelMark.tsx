d57d607d71a2a4f490c353eaf931256a63024a09
Audun Hilden <audunhilden@live.no>
2023-01-03 16:10:25 +0100

    updated footer link

--- src/common/components/Footer/components/VercelMark.tsx
+++ src/common/components/Footer/components/VercelMark.tsx
@@ -4,7 +4,7 @@ function VercelMark() {
   return (
     <div className="flex justify-start text-sm md:justify-center whitespace-nowrap sm:block">
       <a
-        href="https://www.vercel.com/?utm_source=ariakit&amp;utm_campaign=oss"
+        href="https://www.vercel.com/?utm_source=align-items.center&amp;utm_campaign=oss"
         target="_blank"
         rel="noreferrer"
         className="inline-flex items-center gap-2 rounded-sm dark:text-white/70 dark:hover:text-white focus-visible:ariakit-outline hover:no-underline"
